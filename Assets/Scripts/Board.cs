c158f3a1257e3bc4a491a164142f0166846caece
Jay Fernandes <woff27@gmail.com>
2014-09-13 19:37:21 -0700

    Got the CA working

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,59 +3,26 @@ using System.Collections;
 
 public class Board : MonoBehaviour {
 
-	public int boardW; //used for initial board width (in Inspector)
-	public int boardH; //used for initial board height (in Inspector)
+	private int boardW = 7; //used for initial board width (in Inspector)
+	private int boardH = 7; //used for initial board height (in Inspector)
 	private float bX; //position in x of grid blocks
 	private float bY; //posiiton in y of grid blocks
 	private int i; //for loop grid creation, width
 	private int v; //for loop board creation, height
 	public GameObject grid; //Prefab (set in inspector)
 	public Material sSquare; //Green material for starting square
+	public Material BlankGrid; // blank one
 	public int startLimit; //How many times you can click to create a start block
 	private int startCount; //Current amount of start blocks
-	public Vector3[,] gridArray = new Vector3[5,5]; //2D array for newgrid positions
-	public GameObject[,] gridList = new GameObject[5,5]; //2d array for gameobjects themselves
+	public Vector3[,] gridArray = new Vector3[7,7]; //2D array for newgrid positions
+	public GameObject[,] gridList = new GameObject[7,7]; //2d array for gameobjects themselves
+	public Component[,] scriptList = new Component[7,7]; //2d array for the scripts in each cell
+
 	public GameObject newgrid; //instantiated prefab
 	private int Count; //for tag iteration
-	
-	void Awake () 
-	{
-		//variable setting
-		Count = 1;
-		startCount = 0;
-		bX=0;
-		bY=0;
-		//Board creation for loop
-		for(v=0; v<=boardH-1; v++)
-		{
-			for(i=0; i<=boardW-1; i++)
-			{
-				//moves block over
-				bX+=1.5f;
-				//creating prefab in scene, tags for later ID purposes
-				newgrid = (GameObject)Instantiate(grid, new Vector3 (bX,bY,0), Quaternion.identity);
-				newgrid.transform.tag = Count.ToString();
-				newgrid.transform.localScale = new Vector3(1,1,0.5f);
-				//Arrays for positions and GameObjects
-				gridArray[i,v] = new Vector3(bX,bY,0);
-				gridList[i,v] = newgrid;
-				//count for tags and Debug logs to check Arrays
-				Count++;
-				//Debug.Log(gridList[i,v]);
-				//Debug.Log(gridArray[i,v]);
-			}
-		//moves block up, reset x position of board creation
-		bY+=1.5f;
-		bX=0;
-		}
-	}
+	private int time = 0;
 
-	void Start ()
-	{
-		//nothing yet
-	}
-
-	void Update () 
+	public void MouseDown()
 	{
 		//check if too many start blocks are on
 		if(startCount < startLimit)
@@ -72,39 +39,92 @@ public class Board : MonoBehaviour {
 						if(hit.transform.tag != "sSquare")
 						{
 							//change to green material for start square
-							hit.collider.renderer.material = sSquare;
+							//hit.collider.renderer.material = sSquare;
 							hit.transform.tag = "sSquare";
+							StateScript SS = (StateScript) hit.transform.GetComponent("StateScript");
+							SS.Cell.up = SS.Cell.down = SS.Cell.left = SS.Cell.right = true;
+							
 							startCount++; 
 						}
 					}
 				}
 			}
 		}
-		//Checking for square and changing other squares
-//		for (int x = 0; x < 5; x++)
-//		{
-//			for (int y = 0; y < 5; y++)
-//			{
-//				if(gridList[x,y].transform.tag == "sSquare")
-//				{
-//					if(x<4)
-//					{
-//						gridList[x+2,y].collider.renderer.material = sSquare;
-//					}
-//					if(y<4)
-//					{
-//						gridList[x,(y+2)].collider.renderer.material = sSquare;
-//					}
-//					if(x>0)
-//					{
-//						gridList[(x-2),y].collider.renderer.material = sSquare;
-//					}
-//					if(y>0)
-//					{
-//						gridList[x,(y-2)].collider.renderer.material = sSquare;
-//					}
-//				}
-//			}
-//		}
+	}
+
+	public void BoardCreate()
+	{
+		//variable setting
+		Count = 1;
+		startCount = 0;
+		bX=-3.0f;
+		bY=-1.5f;
+		//Board creation for loop
+		for(v=0; v < boardH; v++)
+		{
+			for(i=0; i < boardW; i++)
+			{
+				//moves block over
+				bX+=1.5f;
+				//creating prefab in scene, tags for later ID purposes
+				newgrid = (GameObject)Instantiate(grid, new Vector3 (bX,bY,0), Quaternion.identity);
+				//newgrid.transform.tag = Count.ToString();
+				newgrid.transform.localScale = new Vector3(1,1,0.5f);
+				//Arrays for positions and GameObjects
+				gridList[i,v] = newgrid;
+				StateScript SS = (StateScript)gridList[i,v].transform.GetComponent("StateScript");
+				scriptList[i,v] = SS;
+
+				Count++;
+			}
+			//moves block up, reset x position of board creation
+			bY+=1.5f;
+			bX=-3.0f;
+		}
+	}
+
+	public void tickCells()
+	{
+		for(int x = 0; x<7; x++)
+		{
+			for(int y = 0; y<7; y++)
+			{
+				gridList[x,y].GetComponent<StateScript>().Surround(x,y);
+			}
+		}
+
+		for(int x = 0; x<7; x++)
+		{
+			for(int y = 0; y<7; y++)
+			{
+				StateScript SS = (StateScript)gridList[x,y].transform.GetComponent<StateScript>();
+				SS.Cell.oldup = SS.Cell.up;
+				SS.Cell.olddown = SS.Cell.down;
+				SS.Cell.oldleft = SS.Cell.left;
+				SS.Cell.oldright = SS.Cell.right;
+			}
+		}
+	}
+
+	public void TimedCellCheck()
+	{
+		time++;
+		if(time>30)
+		{
+			time = 0;
+			tickCells();
+		}
+	}
+
+	void Start ()
+	{
+		BoardCreate();
+	}
+
+	void Update () 
+	{
+		MouseDown();
+		TimedCellCheck();
+
 	}
 }
---
+++ Assets/Scripts/StateScript.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateScript : MonoBehaviour {
+
+	public Material GridChild;
+	public Material BlankGrid;
+	public Material sSquare;
+
+	public State Cell = new State();// Cell will be call for State ie Cell.down etc.
+
+	public int x;
+	public int y;
+	
+	public void StateCheck()
+	{
+		if(Cell.up && Cell.down && Cell.left && Cell.right == false)
+		{
+			this.gameObject.collider.renderer.material = BlankGrid;
+		}
+	
+		if(this.transform.tag == "sSquare")
+		{
+			this.gameObject.collider.renderer.material = sSquare;
+			Cell.up = Cell.down = Cell.left = Cell.right = true;
+		}
+	}
+
+	public void Surround(int x, int y)
+	{
+		Board gC = (Board)GameObject.Find("Controller").GetComponent("Board");
+		GameObject[,] gridList = gC.gridList;
+
+		if(y>0)
+		{
+			StateScript SSup = (StateScript)gridList[x,y-1].GetComponent("StateScript");
+			if(SSup.Cell.oldup == true)
+			{
+				this.gameObject.collider.renderer.material = GridChild;
+				Cell.up = true;
+			}
+		}
+		if(y<6)
+		{
+			StateScript SSdown = (StateScript)gridList[x,y+1].GetComponent("StateScript");
+			if(SSdown.Cell.olddown == true)
+			{
+				this.gameObject.collider.renderer.material = GridChild;
+				Cell.down = true;
+			}
+		}
+		if(x<6)
+		{
+			StateScript SSleft = (StateScript)gridList[x+1,y].GetComponent("StateScript");
+			if(SSleft.Cell.oldleft == true)
+			{
+				this.gameObject.collider.renderer.material = GridChild;
+				Cell.left = true;
+			}
+		}
+		if(x>0)
+		{
+			StateScript SSright = (StateScript)gridList[x-1,y].GetComponent("StateScript");
+			if(SSright.Cell.oldright == true)
+			{
+				this.gameObject.collider.renderer.material = GridChild;
+				Cell.right = true;
+			}
+		}
+	}
+
+	//State of on or off bool for each Cell 
+	public class State
+	{
+		public bool up;
+		public bool down;
+		public bool left;
+		public bool right;
+
+		public bool oldup;
+		public bool olddown;
+		public bool oldleft;
+		public bool oldright;
+
+		public State()
+		{
+			up = false;
+			down = false;
+			left = false;
+			right = false;
+
+			oldup = false;
+			olddown = false;
+			oldleft = false;
+			oldright = false;
+		}
+	}
+
+
+
+	void Update()
+	{
+		StateCheck();
+	}
+
+}
